1dac2eaecc988469d1c928f3c1668acc29aebaa8
“komila” <komilakalra@gmail.com>
2023-05-19 23:24:25 +0530

    hello

--- src/main/java/com/knf/dev/demo/controller/UserController.java
+++ src/main/java/com/knf/dev/demo/controller/UserController.java
@@ -1,22 +1,12 @@
 package com.knf.dev.demo.controller;
 
+import com.knf.dev.demo.model.User;
+import com.knf.dev.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.knf.dev.demo.entity.User;
-import com.knf.dev.demo.exception.ResourceNotFoundException;
-import com.knf.dev.demo.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 
 @RestController
 @RequestMapping("/api/v1")
@@ -24,60 +14,60 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
-    // get all user
+    // get all users
     @GetMapping("/users")
-    public List<User> getAllUsers() {
-
+    public List<User> getAllUsers()
+    {
         return userRepository.findAll();
     }
 
     // create user rest API
     @PostMapping("/users")
-    public User createUser(@RequestBody User user) {
+    public Map<String, Boolean> createUser(@RequestBody User user)  {
 
-        return userRepository.save(user);
+        Map<String, Boolean> response = new HashMap<>();
+
+        Boolean bool = userRepository.insert(user) > 0 ?
+                response.put("created", Boolean.TRUE) :
+                response.put("created", Boolean.FALSE);
+
+        return response;
+
     }
 
     // get user by id rest api
     @GetMapping("/users/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
+    public User findUserById(@PathVariable Long id) {
 
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException
-                      ("User not exist with id :" + id));
-
-        return ResponseEntity.ok(user);
+        User user = userRepository.findById(id);
+        return user;
     }
 
-    // update user rest api
-    @PutMapping("/users/{id}")
-    public ResponseEntity<User> updateUser(@PathVariable Long id,
-             @RequestBody User userDetails) {
+   // update user rest api
+   @PutMapping("/users/{id}")
+   public Map<String, Boolean> updateUser(@PathVariable Long id,
+                                          @RequestBody User userDetails) {
 
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException
-                      ("User not exist with id :" + id));
-        user.setFirstName(userDetails.getFirstName());
-        user.setLastName(userDetails.getLastName());
-        user.setEmail(userDetails.getEmail());
-        User updatedUser = userRepository.save(user);
+       userDetails.setId(id);
+       Map<String, Boolean> response = new HashMap<>();
 
-        return ResponseEntity.ok(updatedUser);
+       Boolean bool = userRepository.update(userDetails) > 0 ?
+               response.put("updated", Boolean.TRUE) :
+               response.put("updated", Boolean.FALSE);
+
+      return response;
     }
 
     // delete user rest api
     @DeleteMapping("/users/{id}")
-    public ResponseEntity<Map<String, Boolean>> deleteUser
+    public Map<String, Boolean> deleteUser
                (@PathVariable Long id) {
 
-        User user = userRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException
-                    ("User not exist with id :" + id));
-
-        userRepository.delete(user);
         Map<String, Boolean> response = new HashMap<>();
-        response.put("deleted", Boolean.TRUE);
 
-        return ResponseEntity.ok(response);
+        Boolean bool = userRepository.deleteById(id) > 0 ?
+                response.put("deleted", Boolean.TRUE) :
+                response.put("deleted", Boolean.FALSE);
+        return response;
     }
 }
--- src/main/java/com/knf/dev/demo/model/User.java
+++ src/main/java/com/knf/dev/demo/model/User.java
@@ -1,22 +1,16 @@
-package com.knf.dev.demo.entity;
+package com.knf.dev.demo.model;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "first_name")
     private String firstName;
-    @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email", nullable = false, length = 200)
     private String email;
 
-    public User() {
-        super();
+    public User(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
     }
 
     public Long getId() {
@@ -48,14 +42,6 @@ public class User {
     }
 
     public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public User(String firstName, String lastName,
-          String email) {
-        super();
-        this.firstName = firstName;
-        this.lastName = lastName;
         this.email = email;
     }
 }
